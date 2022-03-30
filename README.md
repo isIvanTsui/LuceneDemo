5930581a188fc94abfae7df11b22b21b09dc555b
isIvanTsui <is.ivan.tsui@gmail.com>
2022-03-30 17:20:12 +0800

    study

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# LuceneDemo
-it's demo about lucene
+<h1 align="center">Lucene 模板</h1>
+
+## 技术栈
+
+* `jdk8`、`maven3.8.1`
+* `SpringBoot2.6.4`、`6.6.0`、`Sqlite`
+
+## 如何启动
+
+1. 把项目拉取下来
+2. 用`idea`打开，自动编译下载完依赖后直接启动即可；无需修改任何配置
+
+## 使用
+
+打开链接：http://localhost:8080/，开始访问
+
+输入关键字 “人参”即可进行全文检索了
+
+![Lucene](https://raw.githubusercontent.com/isIvanTsui/img/master/Lucene.gif)
--- src/main/java/com/ivan/search/controller/SearchController.java
+++ src/main/java/com/ivan/search/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.ivan.search.controller;
 
+import cn.hutool.core.collection.ListUtil;
 import com.ivan.search.component.MyCustomScoreQuery;
 import com.ivan.search.component.MyIkAnalyzer;
 import com.ivan.search.domain.CprMain;
@@ -7,7 +8,7 @@ import com.ivan.search.service.CprContentService;
 import com.ivan.search.service.CprMainService;
 import com.ivan.search.vo.CprVo;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.document.Document;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
 import org.apache.lucene.queries.function.FunctionQuery;
 import org.apache.lucene.queries.function.valuesource.FloatFieldSource;
@@ -20,6 +21,7 @@ import org.apache.lucene.search.highlight.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.NumericUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -31,10 +33,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 搜索控制器
@@ -185,9 +184,61 @@ public class SearchController {
         return tds.scoreDocs[num - 1];
     }
 
-    @GetMapping("/home")
+    void createIndex() throws IOException {
+        List<String> noAnalyzedTitle = ListUtil.list(false, "【药理分类】", "【相关时讯】", "临床指南", "注射剂配伍", "检验值专论", "用药教育", "超说明书用药专论（Off-Label Drug Facts）");
+        List<CprVo> list = cprService.listCpr();
+        // 创建文档的集合
+        Collection<Document> docs = new ArrayList<>();
+        Float score;
+        String content;
+        Random random = new Random();
+        for (CprVo cpr : list) {
+            if (!noAnalyzedTitle.contains(cpr.getCprPhname())) {
+                content = cpr.getCprContent();
+                if ("其他临床应用参考".equals(cpr.getCprPhname())) {
+//                    content = Regex.Replace(content, "([0-9]+)=", "")
+                }
+//
+//                content.AppendFormat("{0} ", cpr.getCprPhname());
+//                sbcontent.Append(content);
+            }
+            // 创建文档对象
+            Document doc = new Document();
+            //StringField会创建索引，但是不会被分词，TextField，即创建索引又会被分词。
+            doc.add(new StoredField("cprid", cpr.getCprid() + ""));
+            doc.add(new TextField("title", cpr.getCprTitle(), Field.Store.YES));
+            doc.add(new TextField("content", cpr.getCprContent(), Field.Store.YES));
+            score = Float.valueOf(random.nextInt(10) + "");
+            //用于追加分值，实现自定义影响最后的计分结果
+            doc.add(new NumericDocValuesField("score_sort", NumericUtils.floatToSortableInt(score)));
+            //用于存储计分结果
+            doc.add(new StoredField("score_store", score));
+            docs.add(doc);
+        }
+
+        // 索引目录类,指定索引在硬盘中的位置，我的设置为D盘的indexDir文件夹
+        Directory directory = FSDirectory.open(FileSystems.getDefault().getPath(luceneIndexPath));
+        // 引入IK分词器
+        MyIkAnalyzer analyzer = new MyIkAnalyzer();
+        // 索引写出工具的配置对象，这个地方就是最上面报错的问题解决方案
+        IndexWriterConfig conf = new IndexWriterConfig(analyzer);
+        //自定义索引中分词的权重及打分情况；如果配置了这个，那么 MyCustomScoreQuery 就会失效
+//        conf.setSimilarity(new MySimilarity());
+        // 设置打开方式：OpenMode.APPEND 会在索引库的基础上追加新索引。OpenMode.CREATE会先清空原来数据，再提交新的索引
+        conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        // 创建索引的写出工具类。参数：索引的目录和配置信息
+        IndexWriter indexWriter = new IndexWriter(directory, conf);
+        //删除之前索引
+        indexWriter.deleteAll();
+        // 把文档集合交给IndexWriter
+        indexWriter.addDocuments(docs);
+        // 提交
+        indexWriter.commit();
+    }
+
+    @GetMapping("/")
     public String home() {
-        return "home.html";
+        return "redirect:home.html";
     }
 
     @GetMapping("sqlite")
